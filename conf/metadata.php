e56b944339a632f4272088939cb6d63262892452
root <root@mschulte-CELSIUS-M460.(none)>
2013-06-01 13:44:12 +0200

    s

--- conf/metadata.php
+++
@@ -1,9 +0,0 @@
-<?php
-$meta['pathtoowncloud']      = array('string');
-$meta['owncloudurl']         = array('string');
-$meta['marginFilelist']      = array('numeric');
-$meta['linkAuthor']	         = array('onoff');
-$meta['allowExternalImages'] = array('onoff');
-$meta['allowedImagesURL']    = array('string');
-$meta['allowedImagesURLregexp'] = array('string');
-$meta['imageboxCaptionStyle']= array('multichoice','_choices' => array('Normal','Italic','Bold'));
